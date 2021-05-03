14a5d5032216e405af04d399005f30eced8172a6
Vishnu Priya VR <papudear@gmail.com>
2021-05-03 11:36:18 +0530

    Tweet Header

--- tweepy_core.py
+++ tweepy_core.py
@@ -53,7 +53,7 @@ WILD_CARDS = [
     "plasma_service",
     "other_city",
 ]
-TWEET_FORMAT = "Pls Help:\nPatient Name: patient_name\nAge: patient_age\nLoc: location\nService Req: service_required\nSpO2: current_spo2_level\nAttendant Name: attendant_name\nContact#: attendant_contact_number\n"
+TWEET_FORMAT = "Please extend help to:\nPatient Name: patient_name\nAge: patient_age\nLoc: location\nService Req: service_required\nSpO2: current_spo2_level\nAttendant Name: attendant_name\nContact#: attendant_contact_number\n"
 TWEET_FOOTER = "**Automated using https://"
 
 
